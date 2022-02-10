0e86ccc3e6d195167ad4108384d8f0af333e3f9e
DanielWestiner <daniel.westiner@gmail.com>
2022-02-09 21:16:39 -0500

    updated Thought model

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: Date.now()
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: 
+      [reactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+
+module.exports = Course;
