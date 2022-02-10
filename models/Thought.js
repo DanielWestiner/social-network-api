94d611f06c9774bd6e69883906da767b7a977890
DanielWestiner <daniel.westiner@gmail.com>
2022-02-09 21:42:42 -0500

    updated Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,34 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+
+// Reaction schema to be used as the reation field's subdoc in Thought model below
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: Date.now()
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
 
 // Schema to create a Thought model
 const thoughtSchema = new Schema(
@@ -28,7 +58,9 @@ const thoughtSchema = new Schema(
   }
 );
 
-thoughtSchema.virtual("reactionCount").get(function() {
+thoughtSchema
+.virtual("reactionCount")
+.get(function() {
   return this.reactions.length;
 });
 
